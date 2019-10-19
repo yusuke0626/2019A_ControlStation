a3dcbb9034b41bb2de31064124fe57e1d3b1b44d
yusuke0626 <m1716@s.akashi.ac.jp>
2019-10-19 16:15:09 +0900

    rgb & depth

--- src/capture.cpp
+++ src/capture.cpp
@@ -16,6 +16,23 @@ constexpr double ratio = WIDTH / (double)HEIGHT;
 constexpr bool filter = true;
 constexpr short hole_fillter_mode = 1;
 
+struct  realsense_distance
+{
+    float rs_x;
+    float rs_y;
+    float rs_z;    
+};
+
+struct rgb_data
+{
+    float x;
+    float y;
+    float z;
+    float yaw;
+    float roll;
+    float pitch;
+};
+
 int main(int argc, char **argv) try
 {
     ros::init(argc, argv, "realsense_info");
@@ -48,8 +65,10 @@ int main(int argc, char **argv) try
     std::chrono::steady_clock::time_point previous_time = std::chrono::steady_clock::now();
 
     // cv::Mat cameraMatrix, distCoeffs;
+    std::array<std::array<float ,4>,5> distance_save;
 
-    float distance_save[5]{0, 0, 0, 0, 0};
+    struct realsense_distance rdist;
+    struct rgb_data rgb;
     //short count = 0;
     short start_count = 0;
     int numbering = 0;
@@ -156,6 +175,7 @@ int main(int argc, char **argv) try
         //        std::vector< cv::Vec3d > rvecs, tvecs;
         //        cv::aruco::estimatePoseSingleMarkers(marker_corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
         //----------------------------------------------------------------------------//
+        
         float sum_marker_coordinate_x = 0;
         float sum_marker_coordinate_z = 0;
         float marker_coodinate_y = 0;
@@ -175,9 +195,16 @@ int main(int argc, char **argv) try
                     }
 
                     cv::aruco::estimatePoseSingleMarkers(marker_corners, 0.272, cameraMatrix, distCoeffs, rvecs,tvecs);
+                    rgb.x = tvecs[marker_num_count].val[0];
+                    rgb.y = tvecs[marker_num_count].val[2];
+                    rgb.z = tvecs[marker_num_count].val[1];
+                    rgb.yaw   = rvecs[marker_num_count].val[2] * 180 / M_PI;
+                    rgb.roll  = rvecs[marker_num_count].val[0] * 180 / M_PI;
+                    rgb.pitch = rvecs[marker_num_count].val[1] * 180 / M_PI;
                     cv::aruco::drawAxis(color, cameraMatrix, distCoeffs, rvecs[marker_num_count], tvecs[marker_num_count], 0.1);
-                    std::cout << tvecs[marker_num_count].val[0]  << "   " << tvecs[marker_num_count].val[1] << "   " << tvecs[marker_num_count].val[2] << std::endl;
-                    std::cout << rvecs[marker_num_count].val[0]  << "   " << rvecs[marker_num_count].val[1] << "   " << rvecs[marker_num_count].val[2] << std::endl;
+
+                    std::cout << rgb.x  << "   " << rgb.y << "   " << rgb.z << std::endl;
+                    std::cout << rgb.yaw << "   " << rgb.roll << "   " << rgb.pitch << std::endl;
                     std::chrono::steady_clock::time_point now_time = std::chrono::steady_clock::now();
                     std::chrono::milliseconds elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(now_time - previous_time);
                     if (elapsed_time.count() > 1)
@@ -185,30 +212,43 @@ int main(int argc, char **argv) try
                         float point_center_marker_x = sum_marker_coordinate_x / 4;
                         float point_center_marker_z = sum_marker_coordinate_z / 4;
 
-                        float marker_distance = 0;
+                        std::array<float,4> marker_info_average;
+                        float depth_average = (depth_map.get_distance(point_center_marker_x, point_center_marker_z) + depth_map.get_distance(point_center_marker_x + 1, point_center_marker_z) + depth_map.get_distance(point_center_marker_x - 1, point_center_marker_z) + depth_map.get_distance(point_center_marker_x, point_center_marker_z - 1) + depth_map.get_distance(point_center_marker_x, point_center_marker_z + 1)) / 5.0;
+                        /*distance_save.at(4)[0] = depth_average;
+                        distance_save.at(4)[1] = rgb.x;
+                        distance_save.at(4)[2] = rgb.y;
+                        distance_save.at(4)[3] = rgb.z;*/
+                        distance_save.at(4) = {depth_average,rgb.x,rgb.y,rgb.z};
 
-                        distance_save[4] = (depth_map.get_distance(point_center_marker_x, point_center_marker_z) + depth_map.get_distance(point_center_marker_x + 1, point_center_marker_z) + depth_map.get_distance(point_center_marker_x - 1, point_center_marker_z) + depth_map.get_distance(point_center_marker_x, point_center_marker_z - 1) + depth_map.get_distance(point_center_marker_x, point_center_marker_z + 1)) / 5.0;
+                        std::cout << " qqq" << std::endl;
                         start_count++;
                         if (start_count > 8)
                         {
-                            marker_distance = (distance_save[0] + distance_save[1] + distance_save[2] + distance_save[3] + distance_save[4]) / 5;
+                            for(int k = 0; k < 4; k++){
+                                marker_info_average.at(k) = (distance_save.at(0)[k] + distance_save.at(1)[k] + distance_save.at(2)[k] + distance_save.at(3)[k] + distance_save.at(4)[k]) / 5;
+                            }
+                            depth_average = marker_info_average.at(0);
+                            rgb.x = marker_info_average.at(1);
+                            rgb.y = marker_info_average.at(2);
+                            rgb.z = marker_info_average.at(3);
+
                             start_count = 8;
                             float point[3] = {0, 0, 0};
                             float pixel[2] = {0, 0};
                             pixel[0] = point_center_marker_x;
                             pixel[1] = point_center_marker_z;
-                            rs2_deproject_pixel_to_point(point, &intr, pixel, marker_distance);
+                            rs2_deproject_pixel_to_point(point, &intr, pixel, depth_average);
                             //double distance_sum = marker_distance + distance_sum;
                             previous_time = now_time;
-                            float center_marker_x = (point[0] * 700 + tvecs[marker_num_count].val[0] * 1300)/2;
-                            float center_marker_y = (point[2] * 700 + tvecs[marker_num_count].val[2] * 1300)/2;
-                            float center_marker_z = (point[1] * 700 + tvecs[marker_num_count].val[1] * 1300)/2;
+                            rdist.rs_x = (point[0] * 1000 + rgb.x * 1000) / 2;
+                            rdist.rs_y = (point[2] * 1000 + rgb.y * 1000) / 2;
+                            rdist.rs_z = (point[1] * 1000 + rgb.z * 1000) / 2;
 
-                            log << center_marker_x << "," << center_marker_y << "," << center_marker_z << std::endl;
-                            ROS_INFO("x:%f  y:%f  z:%f ", center_marker_x, center_marker_y, center_marker_z);
-                            rs_msg.x_distance = center_marker_x;
-                            rs_msg.y_distance = center_marker_y;
-                            rs_msg.z_distance = center_marker_z;
+                            log << rdist.rs_x << "," << rdist.rs_y << "," << rdist.rs_z << std::endl;
+                            ROS_INFO("x:%f  y:%f  z:%f ", rdist.rs_x, rdist.rs_y, rdist.rs_z);
+                            rs_msg.x_distance = rdist.rs_x;
+                            rs_msg.y_distance = rdist.rs_y;
+                            rs_msg.z_distance = rdist.rs_z;
                             ros_realsense_pub.publish(rs_msg);
                         }
                     }
